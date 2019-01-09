52f4d05b5456111107767adbbcd76b97547393d5
vasside27 <43212508+vasside27@users.noreply.github.com>
2019-01-09 19:17:59 +0200

    Update SignUpPage.js

--- PageObjects/SignUpPage.js
+++ PageObjects/SignUpPage.js
@@ -1,25 +1,68 @@
+let assert = require('chai').assert;
 class SignUpPage {
     constructor(driver) {
         this.driver = driver;
-
     }
 
     async findMailButton(){
+        await this.driver.setImplicitWaitTimeout(10000);
         let element = await this.driver.element("id", 'lu.f4a.app.dev:id/email_button');
         await element.click();
-        console.log('HERMAN WAS HERE');
     }
 
-   /*  async findMailinput(locator,mailValue) {
-         await app.elementDisplayed("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.EditText\n");
-         await app.click();
-         await app.setValue(mailValue);
+     async findMailField() {
+         await this.driver.setImplicitWaitTimeout(10000);
+         // await this.driver.setPageLoadTimeout(10000);
+         let element = await this.driver.element("id",'lu.f4a.app.dev:id/email');
+         await element.click();
+         await element.type("roma.zhelizniak@brainbeanapps.com");
      }
 
-    async findGmailButton(locator) {
-        await app.elementDisplayed(locator);
-        await app.click(locator);
+    async acceptLogin() {
+        await this.driver.setImplicitWaitTimeout(10000);
+        let element = await this.driver.element("id","lu.f4a.app.dev:id/button_next");
+        await element.click();
     }
- */
+
+    async clickSignInButton() {
+        await this.driver.setImplicitWaitTimeout(10000);
+        let element = await this.driver.element("id","lu.f4a.app.dev:id/welcome_back_idp_button");
+        await element.click();
+
+    }
+
+    async finalElementFounded() {
+        await this.driver.setImplicitWaitTimeout(10000);
+        let element =  await this.driver.element("id","lu.f4a.app.dev:id/got_it");
+        let elementText = await element.text().then(resp => resp);
+        console.log(elementText);
+        assert.equal(elementText,"GOT",["failed"]);
+    }
+
+
+
+//     a function getd() {
+//         return 'data';
+//     }
+//
+// {
+//     getd()
+//     .then(function(resp) {
+//         const qqq = resp + 'qqq';
+//         return qqq;
+//     })
+//     .catch(err => console.log(err))
+// }
+
+
+
+
+
+
+        // let element = await this.driver.element("id","lu.f4a.app.dev:id/app_name");
+        // console.log(this.driver.element);
+
+
+
 }
 module.exports = SignUpPage;
